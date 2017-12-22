c68565dd877092feee71a4f52e476c9febd9c1ba
Janusch Jacoby <janusch.jacoby@student.hpi.uni-potsdam.de>
2017-12-22 18:12:41 +0100

    Implement longest substring DP

--- src/main/java/utils/CommonSubstring.java
+++ src/main/java/utils/CommonSubstring.java
@@ -22,4 +22,33 @@ public class CommonSubstring {
         }
         return stringA.substring(startIndex, startIndex + maxLength);
     }
+
+    public static String getLongestCommonSubstringDP(String stringA, String stringB) {
+        int m = stringA.length();
+        int n = stringB.length();
+        int longestSuffix[][] = new int[m + 1][n + 1];
+        int maxLength = 0;
+        String longestSubstring = "";
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    longestSuffix[i][j] = 0;
+                }
+                else {
+                    if (stringA.charAt(i - 1) == stringB.charAt(j - 1)) {
+                        longestSuffix[i][j] = longestSuffix[i - 1][j - 1] + 1;
+                        if (longestSuffix[i][j] > maxLength) {
+                            maxLength = longestSuffix[i][j];
+                            longestSubstring = stringA.substring(i - maxLength + 1, i + 1);
+                        }
+                    } else {
+                        longestSuffix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        return longestSubstring;
+    }
 }
